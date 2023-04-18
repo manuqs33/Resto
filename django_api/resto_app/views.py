a719071c74935695b64b58adba82b4718e3a5bc6
manu <you@example.com>
2023-04-18 20:07:27 -0300

    Agrega ViewSets

--- django_api/resto_app/views.py
+++ django_api/resto_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MessageSerializer, EmployeeSerializer, JobApplicationSerializer, JobOpeningSerializer, ReviewSerializer, DishSerializer
+from .models import Message, Employee, Dish, JobApplication, JobOpening, Review
 
-# Create your views here.
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class EmployeeViewSet(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class DishViewSet(ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+
+
+class JobApplicationViewSet(ModelViewSet):
+    queryset = JobApplication.objects.all()
+    serializer_class = JobApplicationSerializer
+
+
+class JobOpeningViewSet(ModelViewSet):
+    queryset = JobOpening.objects.all()
+    serializer_class = JobOpeningSerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+
+
+
+
