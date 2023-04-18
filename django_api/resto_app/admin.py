bbc3a5a4aac17da93b823d88126e96c4e953a972
manu <you@example.com>
2023-04-18 19:31:49 -0300

    Agrega registro al admin y migración de los nuevos models

--- django_api/resto_app/admin.py
+++ django_api/resto_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message, Employee, Dish, JobApplication, JobOpening, Review
 
 # Register your models here.
+admin.site.register(Message)
+admin.site.register(Employee)
+admin.site.register(Dish)
+admin.site.register(JobApplication)
+admin.site.register(JobOpening)
+admin.site.register(Review)
+
