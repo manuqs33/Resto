17c0832c86f0ffe27ba7665ac7afaf4ba437a9c0
manu <you@example.com>
2023-04-18 20:27:17 -0300

    Limita métodos permitidos en los ViewSets, agrega y configura router

--- django_api/resto_app/views.py
+++ django_api/resto_app/views.py
@@ -6,32 +6,37 @@ from .models import Message, Employee, Dish, JobApplication, JobOpening, Review
 class MessageViewSet(ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    http_method_names = ['get', 'post', 'put']
 
 
 class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    http_method_names = ['get', 'post', 'put']
 
 
 class DishViewSet(ModelViewSet):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    http_method_names = ['get', 'post', 'put']
 
 
 class JobApplicationViewSet(ModelViewSet):
     queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
+    http_method_names = ['get', 'post', 'put']
 
 
 class JobOpeningViewSet(ModelViewSet):
     queryset = JobOpening.objects.all()
     serializer_class = JobOpeningSerializer
+    http_method_names = ['get', 'post', 'put']
 
 
 class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
+    http_method_names = ['get', 'post', 'put']
 
 
 
