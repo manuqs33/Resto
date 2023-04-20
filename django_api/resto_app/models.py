ff631594516db7d084ff713af274b353157e73c7
manu <you@example.com>
2023-04-20 19:23:51 -0300

    Agrega el campo recommended a Dish para elegir platos destacados

--- django_api/resto_app/models.py
+++ django_api/resto_app/models.py
@@ -32,6 +32,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    recommended = models.BooleanField()
     dish_type = models.CharField(choices=DISH_TYPE_CHOICES, max_length=20)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
 
