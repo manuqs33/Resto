7ac832628e39273022daa8f4ae153b441b57d837
manu <you@example.com>
2023-04-20 17:45:46 -0300

    Corrige error en el model de Dish. Elimina item y añade description e image a Dish

--- django_api/resto_app/models.py
+++ django_api/resto_app/models.py
@@ -30,9 +30,10 @@ class Dish(models.Model):
     ]
 
     name = models.CharField(max_length=255)
-    item = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.CharField(choices=DISH_TYPE_CHOICES, max_length=20)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
