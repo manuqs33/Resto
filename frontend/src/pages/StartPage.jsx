3406b3c91b0cfc26892467df933035f09d9bd9e9
manu <you@example.com>
2023-04-27 16:25:28 -0300

    Agrega componente Hero a la página de inicio

--- frontend/src/pages/StartPage.jsx
+++ frontend/src/pages/StartPage.jsx
@@ -1,5 +1,8 @@
+import anfitrion from '../assets/anfitrion.webp'
+import { Hero } from '../components/Hero'
+
 export function StartPage() {
   return (
-    <div>StartPage</div>
+    <Hero/>
   )
 }
