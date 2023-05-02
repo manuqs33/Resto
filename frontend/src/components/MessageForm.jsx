33e5446940d0dc989bf9f522c0ae8a88dd59e669
Manuel Quiroga <manuelquirogasoto@gmail.com>
2023-05-02 10:34:25 -0300

    Completa el form para mandar mensajes. Agrega mensaje en caso de éxito

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 export function MessageForm() {
     const [formData, setFormData] = useState({})
+    const [success, setSuccess] = useState(false)
 
     function handleChange(event) {
         const { name, value } = event.target
@@ -11,9 +12,13 @@ export function MessageForm() {
 
     async function handleSubmit(event) {
         event.preventDefault()
-        const response = await axios.post('/api/messages', formData)
+        const response = await axios.post('http://127.0.0.1:8000/messages/', formData)
+        setFormData({})
+        setSuccess(true)
         console.log(response)
     }
+
+    const successMessage = "Su mensaje se ha enviado con éxito."
 
     return (
         <form className="message-form" onSubmit={handleSubmit}>
@@ -22,14 +27,14 @@ export function MessageForm() {
                     <div className="field">
                         <label className="label">Nombre</label>
                         <div className="control">
-                            <input className="input" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Tu nombre" />
+                            <input className="input" type="text" name="name" value={formData.name ?? ''} onChange={handleChange} placeholder="Tu nombre" />
                         </div>
                     </div>
 
                     <div className="field">
                         <label className="label">Dirección</label>
                         <div className="control">
-                            <input className="input" type="text" name="address" value={formData.address} onChange={handleChange} placeholder="Tu dirección" />
+                            <input className="input" type="text" name="address" value={formData.address ?? ''} onChange={handleChange} placeholder="Tu dirección" />
                         </div>
                     </div>
                 </div>
@@ -38,14 +43,14 @@ export function MessageForm() {
                     <div className="field">
                         <label className="label">Email</label>
                         <div className="control">
-                            <input className="input" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+                            <input className="input" type="email" name="email" value={formData.email ?? ''} onChange={handleChange} placeholder="Email" />
                         </div>
                     </div>
 
                     <div className="field">
                         <label className="label">Teléfono</label>
                         <div className="control">
-                            <input className="input" type="tel" name="phone" value={formData.phone} onChange={handleChange} placeholder="Tu número de teléfono" />
+                            <input className="input" type="tel" name="phone" value={formData.phone ?? ''} onChange={handleChange} placeholder="Tu número de teléfono" />
                         </div>
                     </div>
                 </div>
@@ -54,14 +59,14 @@ export function MessageForm() {
             <div className="field">
                 <label className="label">Asunto</label>
                 <div className="control">
-                    <input className="input" type="text" name="subject" value={formData.subject} onChange={handleChange} placeholder="Asunto de tu mensaje" />
+                    <input className="input" type="text" name="subject" value={formData.subject ?? ''} onChange={handleChange} placeholder="Asunto de tu mensaje" />
                 </div>
             </div>
 
             <div className="field">
                 <label className="label">Mensaje</label>
                 <div className="control">
-                    <textarea className="textarea" name="message_text" value={formData.message_text} onChange={handleChange} placeholder="Tu mensaje"></textarea>
+                    <textarea className="textarea" name="message_text" value={formData.message_text ?? ''} onChange={handleChange} placeholder="Tu mensaje"></textarea>
                 </div>
             </div>
 
@@ -72,7 +77,9 @@ export function MessageForm() {
                     </div>
                 </div>
             </div>
-
+            <div>
+                {success && <p>{successMessage}</p>}
+            </div>
         </form>
     )
 }
